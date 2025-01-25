bd1c7985e1d7ed7870066e22776b6b407b849e99
grapeofwrath <69535018+grapeofwrath@users.noreply.github.com>
2025-01-25 00:18:10 -0800

    initial setup

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "NO";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    allSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    packages = forAllSystems ({pkgs}: {
+      default = pkgs.buildGoModule rec {
+        pname = "no";
+        version = "0.0.1";
+
+        src = ./.;
+
+        meta = {
+          description = "A NixOS and Home Manager CLI helper, written in Go";
+          homepage = "https:github.com/grapeofwrath/no";
+          license = pkgs.lib.licenses.gpl3Plus;
+        };
+      };
+    });
+  };
+}
--- main.go
+++ main.go
@@ -8,17 +8,116 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"slices"
 )
+
+type Command struct {
+	Name string
+	Help string
+	Run  func(args []string) error
+}
+
+var commands = []Command{
+	{Name: "rebuild", Help: "Rebuild a NixOS configuration", Run: rebuildCmd},
+	{Name: "update", Help: "Update a flake.lock file", Run: updateCmd},
+	{Name: "help", Help: "Print this help", Run: printHelpCmd},
+}
+
+func printHelpCmd(_ []string) error {
+	flag.Usage()
+	return nil
+}
+
+func rebuildCmd(args []string) error {
+	var dir string
+
+	hostName, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	flagSet := flag.NewFlagSet("rebuild", flag.ExitOnError)
+	flagSet.StringVar(&hostName, "host", hostName, "nixos configuration to use from the flake")
+	flagSet.StringVar(&dir, "dir", ".", "directory containing flake.nix, must be full path")
+	flagSet.Usage = func() {
+		fmt.Fprintln(os.Stderr, `Rebuild a NixOS configuration.
+
+Usage:
+    no rebuild [flags]
+
+Flags:`)
+		flagSet.PrintDefaults()
+		fmt.Fprintln(os.Stderr)
+	}
+	flagSet.Parse(args)
+
+	err = os.Chdir(dir)
+	fmt.Println("Rebuilding NixOS...")
+
+	logFile, err := os.Create(path.Join(dir, "nixos-rebuild.log"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	cmd := exec.Command("nixos-rebuild", "dry-activate", "--flake", ".#"+hostName)
+
+	multiWriter := io.MultiWriter(logFile, os.Stdout)
+
+	cmd.Stdout = multiWriter
+	cmd.Stderr = multiWriter
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func updateCmd(args []string) error {
+	var dir string
+
+	flagSet := flag.NewFlagSet("rebuild", flag.ExitOnError)
+	flagSet.StringVar(&dir, "dir", ".", "directory containing flake.nix, must be full path")
+	flagSet.Usage = func() {
+		fmt.Fprintln(os.Stderr, `Update a 'flake.lock' file.
+
+Usage:
+    no update [flags]
+
+Flags:`)
+		flagSet.PrintDefaults()
+		fmt.Fprintln(os.Stderr)
+	}
+	flagSet.Parse(args)
+
+	err := os.Chdir(dir)
+	fmt.Printf("Updating flake in %s...", dir)
+
+	cmd := exec.Command("nix", "flake", "update")
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
 
 func usage() {
 	intro := `no is a NixOS and Home Manager CLI helper written in Go.
 
-    Usage:
-        no [flags] <command> [command flags]`
+Usage:
+  no [flags] <command> [command flags]`
 	fmt.Fprintln(os.Stderr, intro)
 
 	fmt.Fprintln(os.Stderr, "\nCommands:")
-	// print commands help
+	for _, cmd := range commands {
+		fmt.Fprintf(os.Stderr, "  %-8s %s\n", cmd.Name, cmd.Help)
+	}
 
 	fmt.Fprintln(os.Stderr, "\nFlags:")
 	flag.PrintDefaults()
@@ -28,61 +127,35 @@ func usage() {
 }
 
 func main() {
-	hostName, err := os.Hostname()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// homeDir, err := os.UserHomeDir()
-	// err = os.Chdir(homeDir + "/dotfiles/")
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	flag.StringVar(&hostName, "host", hostName, "Useful if the current hostname differs from the configuration")
-	flag.StringVar(&cwd, "dir", cwd, "Sets the directory for the command to run in")
-
-	rebuildSub := flag.NewFlagSet("rebuild", flag.ExitOnError)
-	rebuildHost := rebuildSub.String("host", hostName, "host")
-
-	updateSub := flag.NewFlagSet("update", flag.ExitOnError)
+	// flag.StringVar(&cwd, "dir", cwd, "Sets the directory for the command to run in")
 
 	flag.Usage = usage
+	flag.Parse()
 
-	if len(os.Args) < 2 {
+	if len(flag.Args()) < 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	err = os.Chdir(cwd)
+	subCmd := flag.Arg(0)
+	subCmdArgs := flag.Args()[1:]
 
-	switch os.Args[1] {
+	runCommand(subCmd, subCmdArgs)
+}
 
-	case "rebuild":
-		rebuildSub.Parse(os.Args[2:])
-		fmt.Println("Rebuilding NixOS for", *rebuildHost, "...")
+func runCommand(name string, args []string) {
+	cmdIdx := slices.IndexFunc(commands, func(cmd Command) bool {
+		return cmd.Name == name
+	})
 
-		logFile, err := os.Create(path.Join(cwd, "nixos-rebuild.log"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer logFile.Close()
+	if cmdIdx < 0 {
+		fmt.Fprintf(os.Stderr, "command \"%s\" not found\n\n", name)
+		flag.Usage()
+		os.Exit(1)
+	}
 
-		rebuild := exec.Command("nixos-rebuild", "dry-activate", "--flake", ".#"+*rebuildHost)
-
-		multiWriter := io.MultiWriter(logFile, os.Stdout)
-
-		rebuild.Stdout = multiWriter
-		rebuild.Stderr = multiWriter
-
-		err = rebuild.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
-	case "update":
-		updateSub.Parse(os.Args[2:])
-		fmt.Println("Updating flake.lock...")
+	if err := commands[cmdIdx].Run(args); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
+		os.Exit(1)
 	}
 }
