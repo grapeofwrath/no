475a4ec2da3b103f9c36284f73a4982becd9eb49
grapeofwrath <69535018+grapeofwrath@users.noreply.github.com>
2025-01-25 14:51:44 -0800

    added garbage subcommand, adjusted subcommand flags

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"os/exec"
-	"path"
 	"slices"
 )
+
+var err error
 
 type Command struct {
 	Name string
@@ -18,6 +18,7 @@ type Command struct {
 }
 
 var commands = []Command{
+	{Name: "garbage", Help: "Run garbage collection and remove old generations", Run: garbageCmd},
 	{Name: "rebuild", Help: "Rebuild a NixOS configuration", Run: rebuildCmd},
 	{Name: "update", Help: "Update a flake.lock file", Run: updateCmd},
 	{Name: "help", Help: "Print this help", Run: printHelpCmd},
@@ -28,8 +29,49 @@ func printHelpCmd(_ []string) error {
 	return nil
 }
 
+func garbageCmd(args []string) error {
+
+	flagSet := flag.NewFlagSet("garbage", flag.ExitOnError)
+	flagSet.Usage = func() {
+		fmt.Fprintln(os.Stderr, `Run garbage collection and remove old generations.
+
+Usage:
+    no garbage [flags]
+
+Flags:
+    -h, --help  Print this help.`)
+		fmt.Fprintln(os.Stderr)
+	}
+	flagSet.Parse(args)
+
+	fmt.Println("Starting system cleanup...")
+
+	trashCmd := exec.Command("nix-collect-garbage", "-d")
+
+	trashCmd.Stdout = os.Stdout
+	trashCmd.Stderr = os.Stdout
+
+	err = trashCmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	profileCmd := exec.Command("sudo", "nix", "profile", "wipe-history", "--profile", "/nix/var/nix/profiles/system", "--older-than", "7d")
+
+	profileCmd.Stdout = os.Stdout
+	profileCmd.Stderr = os.Stdout
+
+	err = profileCmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
 func rebuildCmd(args []string) error {
 	var dir string
+	var activate = "switch"
 
 	hostName, err := os.Hostname()
 	if err != nil {
@@ -37,16 +79,51 @@ func rebuildCmd(args []string) error {
 	}
 
 	flagSet := flag.NewFlagSet("rebuild", flag.ExitOnError)
-	flagSet.StringVar(&hostName, "host", hostName, "nixos configuration to use from the flake")
-	flagSet.StringVar(&dir, "dir", ".", "directory containing flake.nix, must be full path")
+
+	flagSet.StringVar(&hostName, "config", hostName, "nixos configuration to use")
+	flagSet.StringVar(&hostName, "c", hostName, "nixos configuration to use")
+
+	flagSet.StringVar(&dir, "directory", ".", "run in this dir")
+	flagSet.StringVar(&dir, "d", ".", "run in this dir")
+
+	flagSet.Func("activate", "rebuild activation", func(flagValue string) error {
+		for _, allowedValue := range []string{"boot", "dry-activate", "switch"} {
+			if flagValue == allowedValue {
+				activate = flagValue
+				return nil
+			}
+		}
+		fmt.Fprintf(os.Stderr, `must be one of: "boot", "dry-activate", "switch"\n\n`)
+		flagSet.Usage()
+		os.Exit(1)
+
+		return nil
+	})
+	flagSet.Func("a", "rebuild activation", func(flagValue string) error {
+		for _, allowedValue := range []string{"boot", "dry-activate", "switch"} {
+			if flagValue == allowedValue {
+				activate = flagValue
+				return nil
+			}
+		}
+		fmt.Fprintf(os.Stderr, `must be one of: "boot", "dry-activate", "switch"\n\n`)
+		flagSet.Usage()
+		os.Exit(1)
+
+		return nil
+	})
+
 	flagSet.Usage = func() {
 		fmt.Fprintln(os.Stderr, `Rebuild a NixOS configuration.
 
 Usage:
     no rebuild [flags]
 
-Flags:`)
-		flagSet.PrintDefaults()
+Flags:
+    -a, --activate   STRING  Set how the rebuild will be activated. (default 'switch')
+    -c, --config     STRING  Specify which nixos configuration. (default 'hostname')
+    -d, --directory  STRING  Run in this directory, must be full path. (default '.')
+    -h, --help               Print this help.`)
 		fmt.Fprintln(os.Stderr)
 	}
 	flagSet.Parse(args)
@@ -54,18 +131,21 @@ Flags:`)
 	err = os.Chdir(dir)
 	fmt.Println("Rebuilding NixOS...")
 
-	logFile, err := os.Create(path.Join(dir, "nixos-rebuild.log"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logFile.Close()
+	// Not sure if I want the log file, I like the default output better than piped
+	// logFile, err := os.Create(path.Join(dir, "nixos-rebuild.log"))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer logFile.Close()
 
-	cmd := exec.Command("nixos-rebuild", "dry-activate", "--flake", ".#"+hostName)
+	cmd := exec.Command("sudo", "nixos-rebuild", activate, "--flake", ".#"+hostName)
 
-	multiWriter := io.MultiWriter(logFile, os.Stdout)
-
-	cmd.Stdout = multiWriter
-	cmd.Stderr = multiWriter
+	// multiWriter := io.MultiWriter(logFile, os.Stdout)
+	//
+	// cmd.Stdout = multiWriter
+	// cmd.Stderr = multiWriter
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
 
 	err = cmd.Run()
 	if err != nil {
@@ -77,33 +157,62 @@ Flags:`)
 
 func updateCmd(args []string) error {
 	var dir string
+	var switchBool bool
+
+	hostName, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	flagSet := flag.NewFlagSet("rebuild", flag.ExitOnError)
-	flagSet.StringVar(&dir, "dir", ".", "directory containing flake.nix, must be full path")
+
+	flagSet.StringVar(&dir, "directory", ".", "run in this dir")
+	flagSet.StringVar(&dir, "d", ".", "run in this dir")
+
+	flagSet.BoolVar(&switchBool, "switch", false, "switch after update")
+	flagSet.BoolVar(&switchBool, "s", false, "switch after update")
+
 	flagSet.Usage = func() {
 		fmt.Fprintln(os.Stderr, `Update a 'flake.lock' file.
 
 Usage:
     no update [flags]
 
-Flags:`)
-		flagSet.PrintDefaults()
+Flags:
+    -d, --directory  STRING  Run in this directory, must be full path. (default '.')
+    -s, --switch     BOOL    Rebuild system config and activate on boot. (default 'false')
+    -h, --help               Print this help.`)
 		fmt.Fprintln(os.Stderr)
 	}
 	flagSet.Parse(args)
 
-	err := os.Chdir(dir)
+	err = os.Chdir(dir)
 	fmt.Printf("Updating flake in %s...", dir)
 
-	cmd := exec.Command("nix", "flake", "update")
+	updateCmd := exec.Command("sudo", "nix", "flake", "update")
 
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stdout
+	updateCmd.Stdout = os.Stdout
+	updateCmd.Stderr = os.Stdout
 
-	err = cmd.Run()
+	err = updateCmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if switchBool == true {
+		fmt.Println("Rebuilding NixOS...")
+
+		switchCmd := exec.Command("sudo", "nixos-rebuild", "boot", "--flake", ".#"+hostName)
+
+		switchCmd.Stdout = os.Stdout
+		switchCmd.Stderr = os.Stdout
+
+		err = switchCmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	return nil
 }
 
